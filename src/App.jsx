541befda9a06a98c2793c1bb1081fff6e0e39db5
murtadhoaaishah <aishatmurtadho@gmail.com>
2023-02-09 08:12:24 +0100

    add form field

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,46 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const initialNote = [
+    'hello', 'good morning'
+  ]
+  const [notes, setNotes] = useState(initialNote)
+
+  const [newNote, setNewNote] = useState("")
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // if (!newNote.trim()) return
+    setNotes(notes.concat(newNote))
+    setNewNote('')
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+
+      <form >
+        <p className='note-hdn'>Add note </p>
+        Add Not: {""}
+        <input
+          type="text"
+          value={newNote}
+          onChange={e => setNewNote(e.target.value)}
+        />
+
+        <button type='submit' onClick={handleSubmit}>Add</button>
+        {
+          notes.map((note, nameIndex) => <div>
+            <li>
+              <ul>{note}</ul>
+            </li>
+          </div>)
+        }
+        {notes}
+      </form>
+
     </div>
   )
 }
